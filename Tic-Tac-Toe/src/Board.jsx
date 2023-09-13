a511b85182f502d25fc1d1a4b8293f4a589f7c09
Harshit Shankhdhar <harshitshankhdhar01@gmail.com>
2023-09-14 02:03:24 +0530

    Stage 1 : Game is ready!

--- Tic-Tac-Toe/src/Board.jsx
+++ Tic-Tac-Toe/src/Board.jsx
@@ -1,17 +1,36 @@
 import Square from "./square";
-import { useState } from "react";
+import Result from "./Result";
+import { useState } from "react"; // state is private to a component that defines it
+
+let isPlayerOne = true;
+let isGameOver = false;
+let winner = null;
 
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
+    
 
     function clickHandler(i) {
-        const nextSquares = squares.slice();
-        nextSquares[i] = "X";
-        setSquares(nextSquares);
+        if(!isGameOver) {
+            const nextSquares = squares.slice();
+            if(nextSquares[i] == null){
+                nextSquares[i] = isPlayerOne? "X" : "O";
+                setSquares(nextSquares);
+                isPlayerOne = !isPlayerOne;
+                winner = findWinner(nextSquares);
+                if(winner != null){
+                    isGameOver = !isGameOver;
+                    // alert('WIN!!!');
+                    // console.log("Win");
+                }
+            } // this check will not re-render the board if the value for the clicked square is already set
+        }
     }
-    // state is private to a component that defines it
+    if (winner != null) {
+        return <Result value = {winner}/>
+    }
     return(
-        <>
+        <>  <p className = 'header'>Tic-Tac-Toe</p><br />    
             <Square value = {squares[0]} onClick={() => clickHandler(0)}/>
             <Square value = {squares[1]} onClick={() => clickHandler(1)}/>
             <Square value = {squares[2]} onClick={() => clickHandler(2)}/><br/>
@@ -23,4 +42,18 @@ export default function Board() {
             <Square value = {squares[8]} onClick={() => clickHandler(8)}/>
         </>
     );
+}
+
+function findWinner(squares) {
+    const winCases = [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+
+    for (let index = 0; index < winCases.length; index++) {
+        const [a,b,c] = winCases[index];
+        if (squares[a]  === squares[b]  && squares[b] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
 }
---
+++ Tic-Tac-Toe/src/Result.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+export default function Result({value}) {
+    return (
+    <>
+        <div><h2>Winner is {value}</h2></div>
+    </>
+    )
+}
